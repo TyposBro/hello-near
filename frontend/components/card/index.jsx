bee1b4282b04947ac76988c1a45b50ee5c9357f4
Azizbek <khasanjonovich@gmail.com>
2023-05-20 10:18:59 +0900

    foo

--- frontend/components/card/index.jsx
+++ frontend/components/card/index.jsx
@@ -1,7 +1,23 @@
-import { _container } from "./index.styled";
+import { _container, _row, _col, _avatar, _span, _subtitle, _title } from "./index.styled";
 
 const Card = () => {
-  return <_container>index</_container>;
+  return (
+    <_container>
+      <_row>
+        <img src="" alt="avatar" />
+      </_row>
+      <_row>
+        <div>
+          <div>Current company</div>
+          <div>SWE</div>
+          <div>Instagram</div>
+        </div>
+        <div>
+          <div>48 months</div>
+        </div>
+      </_row>
+    </_container>
+  );
 };
 
 export default Card;
