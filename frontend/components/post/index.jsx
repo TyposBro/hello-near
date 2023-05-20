c33c15cd74a1d04235fb09c96ec8b53c8a8c9b7c
uni20192017 <kobeisinov@unist.ac.kr>
2023-05-20 13:15:05 +0900

    skelet

--- frontend/components/post/index.jsx
+++ frontend/components/post/index.jsx
@@ -1,5 +1,16 @@
-const Post = () => {
-  return <div>Post</div>;
+import React from 'react';
+import {PostCard, Avatar, Job, Company, Months, Rating} from "./index.styled";
+
+const PostActivityCard = () => {
+  return (
+    <PostCard>
+      <Avatar>Avatar</Avatar>
+      <Job>Job</Job>
+      <Company>Company</Company>
+      <Months>Months</Months>
+      <Rating>Rating</Rating>
+    </PostCard>
+  );
 };
 
-export default Post;
+export default PostActivityCard;
---
+++ frontend/components/post/index.styled.js
@@ -0,0 +1,14 @@
+import exp from "constants";
+import styled from "styled-components";
+
+export const PostCard= styled.div`
+  background-color: white;
+  padding: 20px 20px;
+`;
+export const _row = styled.div``;
+export const _col = styled.div``;
+
+export const _title = styled.h2``;
+export const _subtitle = styled.div``;
+export const _span = styled.span``;
+export const _avatar = styled.img``;
